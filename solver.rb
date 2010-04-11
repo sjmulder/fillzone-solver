c03d1e5c28f7f6a8bb14c15e102fdf89007c91df
Sijmen Mulder <sjmulder@gmail.com>
2010-04-11 12:53:21 +0100

    Added copyright header.

--- solver.rb
+++ solver.rb
@@ -1,3 +1,14 @@
+# FillzoneSolver
+# A simple solver for the Fillzone game
+# Sijmen Mulder, April 2010
+#
+# I, the copyright holder of this work, hereby release it into the public
+# domain. This applies worldwide.
+#
+# In case this is not legally possible: I grant anyone the right to use this
+# work for any purpose, without any conditions, unless such conditions are
+# required by law.
+
 require 'yaml'
 
 class FillzoneBoard
